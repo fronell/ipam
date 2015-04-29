28d0a872347fb86d4af280fe751715cdae6e771e
Chris Wornell <fro.888@gmail.com>
2015-04-29 05:32:11 +0000

    IP's will automagically be created when network is created

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,5 +1,15 @@
+require 'ipaddr'
+
 class Network < ActiveRecord::Base
   has_many :ips, dependent: :destroy
+
+  # Automatically create all the IP's associated with the new network
+  after_create do |net|
+    IPAddr.new(net[:cidr_address]).to_range.each do |i|
+      # The IP's need to be saved so they're committed to the DB
+      net.ips.new(ip: i, occupado:false).save
+    end
+  end
 
   # Instance methods
   def gimme_ips(amount)
