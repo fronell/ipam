fe62059e2dbb670343c679fd9e97a005613277bb
Chris Wornell <fro.888@gmail.com>
2015-04-30 07:42:23 +0000

    Updated sample_cmds.sh

--- sample_cmds.sh
+++ sample_cmds.sh
@@ -1,12 +1,14 @@
 # Create network
 ## Parametized POST
 curl http://localhost:4000/networks -d 'network[cidr_address]=192.168.1.0/24'
+## JSON POST - We have to specify the model name at the beginning of the JSON
+curl http://localhost:4000/networks.json -H "Content-type: application/json" -d '{ "network": {"cidr_address": "192.168.2.0"} }'
 
 # Update occupado status of IP
-# Parametized PATCH
+## Parametized PATCH
 curl http://localhost:4000/ips/1 -X PATCH -d 'ip[occupado]=true'
-# JSON PATCH
-curl http://localhost:4000/ips/1 -H "Content-type: application/json" -X PATCH -d '{ "ip": {"occupado":"false"} }'
+## JSON PATCH - We don't have to specify the model name in the JSON
+curl http://localhost:4000/ips/1.json -H "Content-type: application/json" -X PATCH -d '{"occupado":"false"}'
 
 # Delete a network
 curl http://localhost:4000/networks/3 -X DELETE
